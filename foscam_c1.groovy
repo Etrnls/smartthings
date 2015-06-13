4e719082a24b38048f5c77c7d93719265e1a3af4
Shuai Wang <shuaiwang@google.com>
2015-06-13 09:11:39 -0700

    cleanup & format

--- foscam_c1.groovy
+++ foscam_c1.groovy
@@ -30,9 +30,9 @@ metadata {
             state "default", icon: "st.camera.dropcam-centered", backgroundColor: "#FFFFFF"
         }
 
-		carouselTile("cameraDetails", "device.image", width: 3, height: 2) { }
+        carouselTile("cameraDetails", "device.image", width: 3, height: 2) { }
 
-		standardTile("take", "device.image", width: 1, height: 1) {
+        standardTile("take", "device.image", width: 1, height: 1) {
             state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.dropcam", backgroundColor: "#FFFFFF", nextState: "taking"
             state "taking", label: "Taking", action: "", icon: "st.camera.dropcam", backgroundColor: "#53a7c0"
             state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.dropcam", backgroundColor: "#FFFFFF", nextState: "taking"
@@ -44,81 +44,55 @@ metadata {
 }
 
 def parse(String description) {
-	log.trace "parse(description = '${description}')"
-
-
-	def map = stringToMap(description)
-	log.debug map
-
-	def result = []
-
-	if (map.bucket && map.key)
-	{
-		putImageInS3(map)
-	}
-	else if (map.headers && map.body)
-	{
-		
-	}
-
-	result
+    def descMap = stringToMap(description)
+    if (descMap.bucket && descMap.key) {
+        putImageInS3(descMap.bucket, descMap.key)
+    } else {
+        log.warn "parse(descMap = '${descMap}') not handled"
+    }
 }
 
-def putImageInS3(map) {
-log.trace "putImageInS3"
-
-	def s3ObjectContent
-
-	try {
-		def imageBytes = getS3Object(map.bucket, map.key + ".jpg")
-
-		if(imageBytes)
-		{
-			s3ObjectContent = imageBytes.getObjectContent()
-			def bytes = new ByteArrayInputStream(s3ObjectContent.bytes)
-			storeImage(getPictureName(), bytes)
-            log.debug "storeImage done"
-		}
-	}
-	catch(Exception e) {
-		log.error e
-	}
-	finally {
-		if (s3ObjectContent) { s3ObjectContent.close() }
-	}
+def putImageInS3(bucket, key) {
+    log.trace "putImageInS3(${bucket}, ${key})"
+    def s3ObjectContent
+    try {
+        def imageBytes = getS3Object(bucket, key + ".jpg")
+        if (imageBytes) {
+            s3ObjectContent = imageBytes.getObjectContent()
+            def bytes = new ByteArrayInputStream(s3ObjectContent.bytes)
+            storeImage(getPictureName(), bytes)
+        }
+    } catch(Exception e) {
+        log.error e
+    } finally {
+        if (s3ObjectContent) { s3ObjectContent.close() }
+    }
 }
 
-// handle commands
 def take() {
-	log.debug "Executing 'take'"
-    
-	def hubAction = new physicalgraph.device.HubAction(
-		method: "GET",
-		path: "/cgi-bin/CGIProxy.fcgi",
-		headers: [HOST: getHostAddress()],
+    log.trace "take()"
+    def hubAction = new physicalgraph.device.HubAction(
+        method: "GET",
+        path: "/cgi-bin/CGIProxy.fcgi",
+        headers: [HOST: getHostAddress()],
         query: [cmd: "snapPicture2", usr: settings.username, pwd: settings.password],
-	)
-	hubAction.options = [outputMsgToS3:true]
+    )
+    hubAction.options = [outputMsgToS3: true]
     log.debug hubAction
-	hubAction
+    hubAction
 }
 
 private getPictureName() {
-	def pictureUuid = java.util.UUID.randomUUID().toString().replaceAll('-', '')
-	return device.deviceNetworkId + "_$pictureUuid" + ".jpg"
-}
-
-private Integer convertHexToInt(hex) {
-	Integer.parseInt(hex,16)
-}
-
-private String convertHexToIP(hex) {
-	[convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
+    def pictureUuid = java.util.UUID.randomUUID().toString().replaceAll('-', '')
+    return device.deviceNetworkId + "_${pictureUuid}.jpg"
 }
 
 private getHostAddress() {
-	def parts = device.deviceNetworkId.split(":")
-	def ip = convertHexToIP(parts[0])
-	def port = convertHexToInt(parts[1])
-	return ip + ":" + port
+    def parts = device.deviceNetworkId.split(":")
+    def ip = [Integer.parseInt(parts[0][0..1], 16),
+              Integer.parseInt(parts[0][2..3], 16),
+              Integer.parseInt(parts[0][4..5], 16),
+              Integer.parseInt(parts[0][6..7], 16)].join(".")
+    def port = Integer.parseInt(parts[1], 16)
+    return ip + ":" + port
 }
